c08a60e4d7ec6e3a95e0866b1848c9161e99085d
Zam <muxa63@gmail.com>
2024-11-06 22:24:48 +0400

    PP1.1.4a

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,56 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private SessionFactory sessionFactory;
 
+    public UserDaoHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
 
     @Override
     public void createUsersTable() {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.createNativeQuery("CREATE TABLE users (id BIGINT PRIMARY KEY, name VARCHAR(255), last_name VARCHAR(255), age TINYINT)").executeUpdate();
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.createNativeQuery("DROP TABLE users").executeUpdate();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = sessionFactory.getCurrentSession();
+        User user = new User(0, name, lastName, age);
+        session.saveOrUpdate(user);
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        User user = session.get(User.class, id);
+        if (user != null) {
+            session.delete(user);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("FROM User", User.class).getResultList();
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.createQuery("DELETE FROM User").executeUpdate();
     }
 }
