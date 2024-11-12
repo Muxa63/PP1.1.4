968fdc105fcd65e7a119e87016c9760eb49e2fba
Zam <muxa63@gmail.com>
2024-11-12 19:04:22 +0400

    PP1.1.4b

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -13,44 +14,104 @@ public class UserDaoHibernateImpl implements UserDao {
         this.sessionFactory = sessionFactory;
     }
 
-
     @Override
     public void createUsersTable() {
         Session session = sessionFactory.getCurrentSession();
-        session.createNativeQuery("CREATE TABLE users (id BIGINT PRIMARY KEY, name VARCHAR(255), last_name VARCHAR(255), age TINYINT)").executeUpdate();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.createNativeQuery("CREATE TABLE users (id BIGINT PRIMARY KEY, name VARCHAR(255), last_name VARCHAR(255), age TINYINT)").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
         Session session = sessionFactory.getCurrentSession();
-        session.createNativeQuery("DROP TABLE users").executeUpdate();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.createNativeQuery("DROP TABLE users").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Session session = sessionFactory.getCurrentSession();
-        User user = new User(0, name, lastName, age);
-        session.saveOrUpdate(user);
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            User user = new User(0L, name, lastName, age);
+            session.saveOrUpdate(user);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
         Session session = sessionFactory.getCurrentSession();
-        User user = session.get(User.class, id);
-        if (user != null) {
-            session.delete(user);
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("FROM User", User.class).getResultList();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            List<User> users = session.createQuery("FROM User", User.class).getResultList();
+            transaction.commit();
+            return users;
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
         Session session = sessionFactory.getCurrentSession();
-        session.createQuery("DELETE FROM User").executeUpdate();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.createQuery("DELETE FROM User").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
